133871ea07eb29a2633cfefaca4a6d266efac6f3
antonio-dev-frontend <antonio.junior@corebiz.ag>
2022-11-08 23:21:38 -0300

    Página de confirmação funcionando

--- src/pages/SuccessPage/SuccessPage.tsx
+++ src/pages/SuccessPage/SuccessPage.tsx
@@ -1,6 +1,10 @@
 import { useContext } from "react";
 import { OrdersContext } from "../../context/OrdersContext";
 import { SuccessPageStyle } from "./styles";
+import imgPoint from '../../assets/pinpoint-white.png'
+import imgClock from '../../assets/clock.png'
+import imgDollar from '../../assets/dolar-icon-order-confirm.png'
+import imgDeliveryMan from '../../assets/delivery-illustration.png'
 
 export function SuccessPage() {
 
@@ -10,7 +14,47 @@ export function SuccessPage() {
 
   return (
     <SuccessPageStyle>
-      <h1>Success Page</h1>
+      <div className="order-info">
+        <h2 className="order-confirm-msg">Uhu! Pedido confirmado</h2>
+        <p className="order-confirm-sub">Agora é só aguardar que logo o café chegará até você</p>
+        <div className="order-summary-info">
+          <div className="line">
+            <div className="icon">
+              <img src={imgPoint} alt="" />
+            </div>
+            <div className="text">
+              <p>
+                Entrega em <strong> Rua João Daniel Martinelli, 102</strong> <br /> Farrapos - Porto Alegre, RS
+              </p>
+            </div>
+          </div>
+          <div className="line">
+            <div className="icon">
+              <img src={imgClock} alt="" />
+            </div>
+            <div className="text">
+              <p>
+                Previsão de entrega <br />
+                <strong> 20 min - 30 min </strong>
+              </p>
+            </div>
+          </div>
+          <div className="line">
+            <div className="icon">
+              <img src={imgDollar} alt="" />
+            </div>
+            <div className="text">
+              <p>
+                Pagamento na entrega <br />
+                <strong> {orderFilled.paymentMode} </strong>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="illustration">
+        <img src={imgDeliveryMan} alt="Delivery Man" />
+      </div>
     </SuccessPageStyle>
   )
 }
--- src/pages/SuccessPage/styles.ts
+++ src/pages/SuccessPage/styles.ts
@@ -3,5 +3,43 @@ import styled from 'styled-components'
 export const SuccessPageStyle = styled.main`
   width: 100%;
   height: 100vh;
-  background-color: red;
+  display: flex;
+
+  .order-info {
+    width: 58%;
+  }
+
+  .illustration {
+    width: 40%;
+  }
+
+  .order-info {
+    .order-confirm-msg {
+      font-family: "Baloo 2", sans-serif;
+      color: ${props => props.theme.yellowDark};
+      font-weight: 800;
+      font-size: 2rem;
+      line-height: 2.6rem;
+    }
+
+    .order-confirm-sub {
+      font-family: "Roboto", sans-serif;
+      color: ${props => props.theme.subtitle};
+      font-weight: 400;
+      font-size: 1.25rem;
+      line-height: 1.6rem;
+    }
+
+    .order-summary-info {
+      box-sizing: content-box;
+      border-width: 1px;
+      border-style: solid;
+      border-image: linear-gradient(102.89deg, #DBAC2C 2.61%, #8047F8 98.76%);
+      width: 100%;
+      height: 100%;
+
+      font-family: "Roboto", sans-serif;
+      font-size: 1rem;
+    }
+  }
 `
