4163d0d194f40610090f84f40fb16f61894d34ad
antonio-dev-frontend <antonio.junior@corebiz.ag>
2022-11-09 23:23:38 -0300

    Pedido confirmado estilizada

--- src/pages/SuccessPage/SuccessPage.tsx
+++ src/pages/SuccessPage/SuccessPage.tsx
@@ -15,39 +15,41 @@ export function SuccessPage() {
   return (
     <SuccessPageStyle>
       <div className="order-info">
-        <h2 className="order-confirm-msg">Uhu! Pedido confirmado</h2>
-        <p className="order-confirm-sub">Agora é só aguardar que logo o café chegará até você</p>
-        <div className="order-summary-info">
-          <div className="line">
-            <div className="icon">
-              <img src={imgPoint} alt="" />
+        <div className="inner">
+          <h2 className="order-confirm-msg">Uhu! Pedido confirmado</h2>
+          <p className="order-confirm-sub">Agora é só aguardar que logo o café chegará até você</p>
+          <div className="order-summary-info">
+            <div className="line">
+              <div className="icon">
+                <img src={imgPoint} alt="" />
+              </div>
+              <div className="text">
+                <p>
+                  Entrega em <strong> {orderFilled.rua}, {orderFilled.numero}</strong> <br /> {orderFilled.cidade} - {orderFilled.uf}
+                </p>
+              </div>
             </div>
-            <div className="text">
-              <p>
-                Entrega em <strong> Rua João Daniel Martinelli, 102</strong> <br /> Farrapos - Porto Alegre, RS
-              </p>
+            <div className="line">
+              <div className="icon">
+                <img src={imgClock} alt="" />
+              </div>
+              <div className="text">
+                <p>
+                  Previsão de entrega <br />
+                  <strong> 20 min - 30 min </strong>
+                </p>
+              </div>
             </div>
-          </div>
-          <div className="line">
-            <div className="icon">
-              <img src={imgClock} alt="" />
-            </div>
-            <div className="text">
-              <p>
-                Previsão de entrega <br />
-                <strong> 20 min - 30 min </strong>
-              </p>
-            </div>
-          </div>
-          <div className="line">
-            <div className="icon">
-              <img src={imgDollar} alt="" />
-            </div>
-            <div className="text">
-              <p>
-                Pagamento na entrega <br />
-                <strong> {orderFilled.paymentMode} </strong>
-              </p>
+            <div className="line">
+              <div className="icon">
+                <img src={imgDollar} alt="" />
+              </div>
+              <div className="text">
+                <p>
+                  Pagamento na entrega <br />
+                  <strong> {orderFilled.paymentMode} </strong>
+                </p>
+              </div>
             </div>
           </div>
         </div>
--- src/pages/SuccessPage/styles.ts
+++ src/pages/SuccessPage/styles.ts
@@ -4,42 +4,99 @@ export const SuccessPageStyle = styled.main`
   width: 100%;
   height: 100vh;
   display: flex;
+  align-items: center;
+  margin-top: 2.5rem;
 
   .order-info {
     width: 58%;
+    margin-right: 2%;
+
+    .inner {
+      padding: 2.81rem 2.5rem;
+
+      background: linear-gradient(white, white) padding-box,
+                  linear-gradient(to right, #DBAC2C, #8047F8) border-box;
+        border-radius: 6px 36px;
+        border: 1px solid transparent;
+
+        .order-confirm-msg {
+          font-family: "Baloo 2", sans-serif;
+          color: ${props => props.theme.yellowDark};
+          font-weight: 800;
+          font-size: 2rem;
+          line-height: 2.6rem;
+        }
+    
+        .order-confirm-sub {
+          font-family: "Roboto", sans-serif;
+          color: ${props => props.theme.subtitle};
+          font-weight: 400;
+          font-size: 1rem;
+          line-height: 1.6rem;
+        }
+    
+        .order-summary-info {
+          box-sizing: content-box;
+          border-width: 1px;
+          border-style: solid;
+          border-image: linear-gradient(102.89deg, #DBAC2C 2.61%, #8047F8 98.76%);
+          width: 100%;
+          height: 100%;
+    
+          font-family: "Roboto", sans-serif;
+          font-size: 1rem;
+    
+          .line {
+            display: flex;
+            margin: 2rem 0;
+          }
+    
+          .line {
+           .icon {
+            padding: 0.5rem;
+            width: 2rem;
+            height: 2rem;
+            border-radius: 50px;
+    
+            display: flex;
+            justify-content: center;
+           }
+    
+           .text {
+            padding-left: 0.75rem;
+           }
+          }
+    
+          .line {
+            &:first-child {
+              .icon {
+                &:first-child {
+                  background-color: ${props => props.theme.purple};
+                }
+              }
+            }
+    
+            &:nth-child(2) {
+              .icon {
+                &:first-child {
+                  background-color: ${props => props.theme.yellow};
+                }
+              }
+            }
+    
+            &:last-child {
+              .icon {
+                &:first-child {
+                  background-color: ${props => props.theme.yellowDark};
+                }
+              }
+            }
+          }
+        }
+    }
   }
 
   .illustration {
     width: 40%;
-  }
-
-  .order-info {
-    .order-confirm-msg {
-      font-family: "Baloo 2", sans-serif;
-      color: ${props => props.theme.yellowDark};
-      font-weight: 800;
-      font-size: 2rem;
-      line-height: 2.6rem;
-    }
-
-    .order-confirm-sub {
-      font-family: "Roboto", sans-serif;
-      color: ${props => props.theme.subtitle};
-      font-weight: 400;
-      font-size: 1.25rem;
-      line-height: 1.6rem;
-    }
-
-    .order-summary-info {
-      box-sizing: content-box;
-      border-width: 1px;
-      border-style: solid;
-      border-image: linear-gradient(102.89deg, #DBAC2C 2.61%, #8047F8 98.76%);
-      width: 100%;
-      height: 100%;
-
-      font-family: "Roboto", sans-serif;
-      font-size: 1rem;
-    }
   }
 `
